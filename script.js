196be24df1818a2c1c65eacad13d15cfef8f9462
azizaliyi <154307123+azizaliyi@users.noreply.github.com>
2024-03-24 11:23:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you next time rabia!";
+  question.innerHTML = "Yay, I miss you too, Thanks";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
